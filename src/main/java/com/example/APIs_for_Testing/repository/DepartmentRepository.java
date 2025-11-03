9887cb2022f330dfc5310197350e8d29ffea365f
deep2002-git <dy345252@gmail.com>
2025-11-03 13:05:17 +0530

    Nothing change

--- src/main/java/com/example/APIs_for_Testing/repository/DepartmentRepository.java
+++ src/main/java/com/example/APIs_for_Testing/repository/DepartmentRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 
-public interface DepartmentRepository extends JpaRepository<DepartmentManual, Long> {
+public interface   DepartmentRepository extends JpaRepository<DepartmentManual, Long> {
 
 }
